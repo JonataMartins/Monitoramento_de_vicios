40c341cb320bdd0a5eb348dd810899faa16a31d2
Davi-Martins-Sousa <74196401+Davi-Martins-Sousa@users.noreply.github.com>
2025-10-25 10:02:31 -0300

    Update habito.js
    incrementa informações adicionais em habitos no banco para unir as tabelas habitos e vicios

--- public/backend/models/habito.js
+++ public/backend/models/habito.js
@@ -14,6 +14,22 @@ const habitoSchema = new mongoose.Schema({
     type: String,
     default: ''
   },
+  tipo:{ // 0 para habitos ruins(vícios) e 1 para habitos bons
+    type: Boolean,
+    required: true
+  },
+  sequencia_atual: {
+    type: Number,
+    default: 0
+  },
+  sequencia_melhor: {
+    type: Number,
+    default: 0
+  },
+  sequencia_media: {
+    type: Number,
+    default: 0
+  },
   data_criacao: {
     type: Date,
     default: Date.now
